f314447b326438eace0cc929b108fe32b56f6bca
AmrHesham <eng.amrhesham.22@gmail.com>
2022-11-05 11:34:59 +0200

    fixing last commit
    useEffect cannot be async to prevent race condition

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,11 +7,15 @@ import Subscripe from "./components/Subscripe/Subscripe";
 function Home() {
   const [products, setProducts] = useState([]);
 
-  useEffect(async () => {
-    const response = await fetch(`${process.env.REACT_APP_SERVER}/products`);
-    const data = await response.json();
-    if (response.ok) setProducts(data["data"]);
-  }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(`${process.env.REACT_APP_SERVER}/products`);
+      const data = await response.json();
+      if (response.ok) setProducts(data["data"]);
+      return;
+    };
+    fetchProducts();
+  }, [setProducts]);
 
   return (
     <Layout>
